a1bf179d2fc3c1a9ab7b3ee03367a62ec1f1acfa
GwendalDubief <50627215+GwendalDubief@users.noreply.github.com>
2019-05-15 16:27:42 +0200

    Ajout scrip modif mdp et ajout de choix du nom sur le bouton profil

--- panel_prof_gesoraux.php
+++ panel_prof_gesoraux.php
@@ -62,7 +62,7 @@
                     <ul class="">
                       <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle text-white" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <button type="button" name="button" class="btn btn-secondary"><span class="mr-2 d-none d-lg-inline text-white extra-large float-right"><i class="fas fa-user-circle"></i> Prof</span></button>
+                          <button type="button" name="button" class="btn btn-secondary"><span class="mr-2 d-none d-lg-inline text-white extra-large float-right"><i class="fas fa-user-circle"></i> <?php echo $_SESSION['nom_prenom']; ?></span></button>
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -70,7 +70,7 @@
                             <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                             Mon profil
                           </a>
-                          <a class="dropdown-item" href="#">
+                          <a class="dropdown-item" href="changement_mot_de_passe.php">
                             <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
                             Modifier mon mot de passe
                           </a>
